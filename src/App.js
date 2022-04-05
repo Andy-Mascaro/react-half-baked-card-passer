e0e2b135eb39c67f99da5c0d1906488284dfa883
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-04-04 17:39:27 -0700

    moved pass card and index into execute function

--- src/App.js
+++ src/App.js
@@ -4,41 +4,45 @@ import Player from './components/Player';
 import CardList from './components/CardList';
 import { useState } from 'react';
 import ExecutePassButton from './components/ExecutePassButton';
+import { useGameContext } from './context/GameContext';
 
 function App() {
-  const [deck, setDeck] = useState(initialCards);
-  const [playerOneHand, setPlayerOneHand] = useState([]);
-  const [selectedCard, setSelectedCard] = useState();
-  const [playerTwoHand, setPlayerTwoHand] = useState([]);
-  const [playerThreeHand, setPlayerThreeHand] = useState([]);
-  const [from, setFrom] = useState('deck');
-  const [to, setTo] = useState(1);
+  const { deck, playerOneHand, playerTwoHand, playerThreeHand, selectedCard, 
+  
+    to, setFrom, setTo, from } = useGameContext();
+  // const [deck, setDeck] = useState(initialCards);
+  // const [playerOneHand, setPlayerOneHand] = useState([]);
+  // const [selectedCard, setSelectedCard] = useState();
+  // const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  // const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  // const [from, setFrom] = useState('deck');
+  // const [to, setTo] = useState(1);
 
-  function findCardIndex(value, suit, cards) {
-    return cards.findIndex((card) => card.value === value && card.suit === suit);
-  }
+  // function findCardIndex(value, suit, cards) {
+  //   return cards.findIndex((card) => card.value === value && card.suit === suit);
+  // }
 
-  function passCard(card) {
-    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
-    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
+  // function passCard(card) {
+  //   const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
+  //   const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
 
-    // arrays start at zero, but our players start at 1 :shrug:
-    const toHand = playerHands[to - 1] || deck;
-    const fromHand = playerHands[from - 1] || deck;
+  //   // arrays start at zero, but our players start at 1 :shrug:
+  //   const toHand = playerHands[to - 1] || deck;
+  //   const fromHand = playerHands[from - 1] || deck;
 
-    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
-    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
+  //   const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
+  //   const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
 
-    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
-    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
+  //   const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
+  //   const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
 
-    toHand.push(cardToMove);
+  //   toHand.push(cardToMove);
 
-    toSetFunction([...toHand]);
-    fromSetFunction([...fromHand]);
+  //   toSetFunction([...toHand]);
+  //   fromSetFunction([...fromHand]);
 
-    setSelectedCard(null);
-  }
+  //   setSelectedCard(null);
+  // }
 
   return (
     <div className="App">
@@ -51,7 +55,7 @@ function App() {
           setFrom={setFrom}
           selectedCard={selectedCard}
           setTo={setTo}
-          setSelectedCard={setSelectedCard}
+          // setSelectedCard={setSelectedCard}
         />
         <Player
           to={to}
@@ -60,7 +64,7 @@ function App() {
           setFrom={setFrom}
           selectedCard={selectedCard}
           setTo={setTo}
-          setSelectedCard={setSelectedCard}
+          // setSelectedCard={setSelectedCard}
         />
         <Player
           to={to}
@@ -69,12 +73,12 @@ function App() {
           setFrom={setFrom}
           selectedCard={selectedCard}
           setTo={setTo}
-          setSelectedCard={setSelectedCard}
+          // setSelectedCard={setSelectedCard}
         />
         <CardList
           cards={deck}
           selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
+          // setSelectedCard={setSelectedCard}
           setFrom={setFrom}
           player={'deck'}
         />
@@ -82,12 +86,11 @@ function App() {
       <section>
         {selectedCard && (
           <ExecutePassButton
-            passCard={passCard}
             setFrom={setFrom}
             from={from}
             to={to}
             selectedCard={selectedCard}
-            setSelectedCard={setSelectedCard}
+            // setSelectedCard={setSelectedCard}
           />
         )}
       </section>
