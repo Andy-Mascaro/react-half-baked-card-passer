6b28e208e52251c2f620932f771dcac06565e95f
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-04-04 18:46:52 -0700

    added const to player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import { useGameContext } from '../context/GameContext';
 import CardList from './CardList';
 
-export default function Player({
-  player,
-  setTo,
-  hand,
-  setFrom,
-  setSelectedCard,
-  selectedCard,
-  to,
-}) {
+export default function Player({ player, hand }) { 
+  const { setTo, setFrom, setSelectedCard, selectedCard, to } = useGameContext();
+
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
